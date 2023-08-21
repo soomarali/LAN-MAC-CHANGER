2bd540ff14be380a1da3d60402527ebbba48dac6
soomarali <40350718+soomarali@users.noreply.github.com>
2023-08-22 00:38:40 +0500

    Update installer.sh

--- Mac_Plugin/installer.sh
+++ Mac_Plugin/installer.sh
@@ -12,23 +12,44 @@
 
 PACKAGE_DIR='MAC-Changer/main/Mac_Plugin'
 
-MY_IPK="enigma2-plugin-extensions-MAC-plugin_v1.0_all.ipk"
-MY_DEB="enigma2-plugin-extensions-MAC-plugin_v1.0_all.deb"
+#MY_IPK="enigma2-plugin-extensions-MAC-plugin_v1.0_all.ipk"
+#MY_IPK2="enigma2-plugin-extensions-MAC-plugin_v1.0_py3.ipk"
+#MY_DEB="enigma2-plugin-extensions-MAC-plugin_v1.0_all.deb"
+#MY_DEB2="enigma2-plugin-extensions-MAC-plugin_v1.0_p3.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
 ########################################################################################################################
+# Get Python version
+python_version=$(python -c 'import sys; print(sys.version_info.major)')
+echo "Detected Python version: $python_version"
+sleep 2
+if [ "$python_version" -lt 3 ]; then
+    echo "Python version: $python_version"
+    sleep 2
+    MY_IPK="enigma2-plugin-extensions-MAC-plugin_v1.0_all.ipk"
+    MY_DEB="enigma2-plugin-extensions-MAC-plugin_v1.0_all.deb"
+else
+    echo "Python version: $python_version"
+    sleep 2
+    MY_IPK="enigma2-plugin-extensions-plugin_v1.0_py3.ipk"
+    MY_DEB="enigma2-plugin-extensions-plugin_v1.0_p3.deb"
+fi
 
+# Display selected files
+echo "Selected files: $MY_IPK, $MY_DEB"
+
+sleep 4
 # Decide : which package ?
 MY_MAIN_URL="https://raw.githubusercontent.com/soomarali/"
 if which dpkg > /dev/null 2>&1; then
-	MY_FILE=$MY_DEB
-	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_DEB
+        MY_FILE=$MY_DEB
 else
 	MY_FILE=$MY_IPK
 	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_IPK
 fi
 MY_TMP_FILE="/tmp/"$MY_FILE
+
 
 echo ''
 echo '************************************************************'
@@ -39,7 +60,7 @@ echo "**                 Devolped For: DREAMWORLD                **"
 echo "**                 SUPPORT     : 03357300604               **"
 echo "************************************************************"
 echo ''
-
+sleep 4
 # Remove previous file (if any)
 rm -f $MY_TMP_FILE > /dev/null 2>&1
 
